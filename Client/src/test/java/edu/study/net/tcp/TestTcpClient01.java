9837adbf5dd2d5733fc4ea0f0880ae060e097978
yli107 <yli107@ncsu.edu>
2021-01-14 14:03:51 +0800

    updated client test case

--- Client/src/test/java/edu/study/net/tcp/TestTcpClient01.java
+++ Client/src/test/java/edu/study/net/tcp/TestTcpClient01.java
@@ -1,28 +1,27 @@
 package edu.study.net.tcp;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.Assert;
 
-class TestTcpClient01 {
+public class TestTcpClient01 {
 
 	private TcpClientIO clientIO;
 
 	@Before
-	void setUp() {
+	public void setUp() {
 		clientIO = new TcpClientIO("10.1.15.113");
 	}
 
 	@Test
-	void test() {
+	public void test() {
 		clientIO.send("String");
 		clientIO.send("String1");
 		clientIO.send("String2");
 		clientIO.send("String3");
 		clientIO.send("~!@#$%^&*()_+|}{:?><,./;'[]\'");
 		clientIO.send("1234567890");
+		assertEquals("10.1.15.113",clientIO.getHostAddress());
 	}
 
 }
